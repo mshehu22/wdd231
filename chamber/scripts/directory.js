45a58dc7fd0ac3af7f77d405c680eb77cc4fa6e8
Magdalene Shehu <lene_06@yahoo.com>
2026-07-10 13:14:10 +0100

    Create Chamber Directory

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,59 @@
+const url = "data/members.json";
+const cards = document.querySelector("#members");
+
+async function getMembers() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMembers(data);
+}
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+
+        let card = document.createElement("section");
+        let name = document.createElement("h3");
+        let address = document.createElement("p");
+        let phone = document.createElement("p");
+        let website = document.createElement("a");
+        let image = document.createElement("img");
+
+        name.textContent = member.name;
+        address.textContent = member.address;
+        phone.textContent = member.phone;
+
+        website.textContent = "Visit Website";
+        website.href = member.website;
+        website.target = "_blank";
+
+        image.setAttribute("src", member.image);
+        image.setAttribute("alt", member.name);
+        image.setAttribute("loading", "lazy");
+        image.setAttribute("width", "200");
+
+        card.appendChild(image);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+
+        cards.appendChild(card);
+    });
+};
+
+getMembers();
+
+document.getElementById("year").textContent =
+new Date().getFullYear();
+
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+
+gridButton.addEventListener("click", () => {
+    cards.classList.add("grid");
+    cards.classList.remove("list");
+});
+
+listButton.addEventListener("click", () => {
+    cards.classList.add("list");
+    cards.classList.remove("grid");
+});
