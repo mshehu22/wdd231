aada27fbea0bcae3fff39e2cdb94012e811c3a35
Magdalene Shehu <lene_06@yahoo.com>
2026-07-14 16:02:39 +0100

    Add chamber directory script and list view

---
+++ scripts/directory.js
@@ -0,0 +1,60 @@
+const membersContainer = document.querySelector("#members");
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+
+async function getMembers() {
+    const response = await fetch("data/members.json");
+    const data = await response.json();
+
+    displayMembers(data.members);
+}
+
+function displayMembers(members) {
+
+    members.forEach(member => {
+
+        const card = document.createElement("section");
+        card.classList.add("card");
+
+        card.innerHTML = `
+            <img src="${member.image}"
+                 alt="${member.name} logo"
+                 loading="lazy">
+
+            <h2>${member.name}</h2>
+
+            <p>${member.address}</p>
+
+            <p>${member.phone}</p>
+
+            <p>
+                Membership Level:
+                ${member.membership}
+            </p>
+
+            <a href="${member.website}" target="_blank">
+                Visit Website
+            </a>
+        `;
+
+        membersContainer.appendChild(card);
+    });
+}
+
+gridButton.addEventListener("click", () => {
+    membersContainer.classList.add("grid");
+    membersContainer.classList.remove("list");
+});
+
+listButton.addEventListener("click", () => {
+    membersContainer.classList.add("list");
+    membersContainer.classList.remove("grid");
+});
+
+document.getElementById("year").textContent =
+    new Date().getFullYear();
+
+document.getElementById("lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
+
+getMembers();
