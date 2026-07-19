78d89c9eabe692f961e98b21cb4811bbb4c8a365
Magdalene Shehu <lene_06@yahoo.com>
2026-07-19 14:24:27 +0100

    Final W02 corrections

--- scripts/directory.js
+++ scripts/directory.js
@@ -1,11 +1,72 @@
-const membersContainer =
-    document.querySelector("#members");
+const url = "data/members.json";
 
-const gridButton =
-    document.querySelector("#grid");
+const cards = document.querySelector("#members");
 
-const listButton =
-    document.querySelector("#list");
+async function getMembers() {
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    displayMembers(data.members);
+}
+
+function displayMembers(members) {
+
+    cards.innerHTML = "";
+
+    members.forEach(member => {
+
+        const card = document.createElement("section");
+
+        card.classList.add("card");
+
+        card.innerHTML = `
+            <img
+                src="images/${member.image}"
+                alt="${member.name} logo"
+                loading="lazy"
+            >
+
+            <h2>${member.name}</h2>
+
+            <p>${member.address}</p>
+
+            <p>${member.phone}</p>
+
+            <a href="${member.website}"
+               target="_blank">
+               Visit Website
+            </a>
+        `;
+
+        cards.appendChild(card);
+    });
+}
+
+getMembers();
+
+document.querySelector("#grid").addEventListener("click", () => {
+
+    cards.classList.add("grid");
+
+    cards.classList.remove("list");
+});
+
+document.querySelector("#list").addEventListener("click", () => {
+
+    cards.classList.add("list");
+
+    cards.classList.remove("grid");
+});
+
+document.getElementById("year").textContent =
+    new Date().getFullYear();
+
+document.getElementById("lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
+
+
 
 const menuButton =
     document.querySelector("#menu");
@@ -14,74 +75,7 @@ const navigation =
     document.querySelector(".navigation");
 
 menuButton.addEventListener("click", () => {
+
     navigation.classList.toggle("open");
-});
-
-async function getMembers() {
-
-    const response =
-        await fetch("data/members.json");
-
-    const data =
-        await response.json();
-
-    displayMembers(data);
-}
-
-function displayMembers(members) {
-
-    members.forEach(member => {
-
-        const card =
-            document.createElement("section");
-
-        card.classList.add("card");
-
-        card.innerHTML = `
-            <img src="${member.image}"
-                 alt="${member.name} logo"
-                 loading="lazy">
-
-            <h2>${member.name}</h2>
-
-            <p>${member.address}</p>
-
-            <p>${member.phone}</p>
-
-            <p>
-                Membership Level:
-                ${member.membershipLevel}
-            </p>
-
-            <a href="${member.website}"
-               target="_blank">
-               Visit Website
-            </a>
-        `;
-
-        membersContainer.appendChild(card);
-    });
-}
-
-gridButton.addEventListener("click", () => {
-
-    membersContainer.classList.add("grid");
-    membersContainer.classList.remove("list");
 
 });
-
-listButton.addEventListener("click", () => {
-
-    membersContainer.classList.add("list");
-    membersContainer.classList.remove("grid");
-
-});
-
-document.querySelector("#year").textContent =
-    new Date().getFullYear();
-
-document.querySelector("#lastModified").textContent =
-    `Last Modified:
-    ${document.lastModified}`;
-
-getMembers();
